e97f3aac0b3e4c3edb8f45d1545c7b9eac361281
fayyazulhassan1 <fayyazulhassan@gmail.com>
2025-01-30 15:38:29 +0500

    Create app.py

---
+++ app.py
@@ -0,0 +1,7 @@
+ ```python
+     from flask import Flask
+
+     app = Flask(__name__)
+     app.run(app_name='Your Application Name', host='localhost', port=5000)
+     ```
+   - Place this file in your project folder.
